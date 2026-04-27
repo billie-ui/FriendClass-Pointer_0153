76b6083ce787c5e115643eff8b9c3b9a9f47ab44
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-04-27 18:04:19 +0700

    menambahkan atribut dan method pada class LayangLayang

--- tugas.cpp
+++ tugas.cpp
@@ -5,3 +5,17 @@ using namespace std;
 class BelahKetupat;
 class LayangLayang
 
+{
+    private:
+        float d1, d2;
+        float sisi1, sisi2;
+    public:
+        void input();
+        float Luas();
+        float Keliling();
+        void output();
+        
+        // Friend function declaration
+        friend class BelahKetupat;
+};
+
