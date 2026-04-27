9fc53a111241fcfc9cb6f684703b97bfbe1bfb0f
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-04-27 18:07:26 +0700

    menyelesaikan method perhitungan dan output LayangLayang

--- tugas.cpp
+++ tugas.cpp
@@ -47,3 +47,24 @@ void LayangLayang::input()
     cin >> sisi2;
 }
 
+float LayangLayang::Luas()
+{
+    return (d1 * d2) / 2;
+}
+
+float LayangLayang::Keliling()
+{
+    return 2 * (sisi1 + sisi2);
+}
+
+void LayangLayang::output()
+{
+    cout << "\n=== Output Layang-Layang ===" << endl;
+    cout << "Diagonal 1: " << d1 << endl;
+    cout << "Diagonal 2: " << d2 << endl;
+    cout << "Sisi 1: " << sisi1 << endl;
+    cout << "Sisi 2: " << sisi2 << endl;
+    cout << "Luas: " << Luas() << endl;
+    cout << "Keliling: " << Keliling() << endl;
+}
+
