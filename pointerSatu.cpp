93150db037f0f8ba83724a1d775c02f830bc9d9f
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-04-27 15:41:21 +0700

    memberikan akses data mahasiswa menggunakan reference dan pointer

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -10,3 +10,19 @@ class mahasiswa
             cout << "No Induk = "<<nim<<endl;
         }
 };
+
+int main ()
+{
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); //member accses operator
+
+    mahasiswa &refMhs = mhs;
+    refMhs.nim = 2;
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;
+    pMhs->nim = 3;
+    pMhs->showNim();
+    return 0;
+}
+
